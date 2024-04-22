1bf7db7e6fcb3859db1e9a7214ced1d9a320efcd
Beaka0717 <minjee9009@gmail.com>
2024-04-22 14:34:12 +0900

    test
    test

---
+++ src/store/about.js
@@ -0,0 +1,8 @@
+import { Store } from '../core/heropy'
+
+export default new Store({
+  photo: 'https://techworld.speedgabia.com/newsletter/2024/ad/minji.ico',
+  name: 'Minj.D',
+  email: 'minjee9009@gmail.com',
+
+})
